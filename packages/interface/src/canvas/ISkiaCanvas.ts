9633e7b1b569ffc39729c14da3cc5f40ab88dc20
ahdg <36436808+ahdg6@users.noreply.github.com>
2023-08-21 20:45:58 +0800

    support napi-rs/canvas

--- packages/interface/src/canvas/ISkiaCanvas.ts
+++ packages/interface/src/canvas/ISkiaCanvas.ts
@@ -1,6 +1,6 @@
 import { IExportFileType, IExportImageType } from '../file/IFileType'
 
-export type ICanvasType = 'skia' | 'canvas' | 'wx'
+export type ICanvasType = 'skia' | 'napi' | 'canvas' | 'wx'
 
 export interface ISkiaCanvas {
     toBuffer(format: IExportFileType, config: ISkiaCanvasExportConfig): Promise<any>
@@ -9,6 +9,19 @@ export interface ISkiaCanvas {
     toDataURLSync(format: IExportImageType, config: ISkiaCanvasExportConfig): string
     saveAs(filename: string, config: ISkiaCanvasExportConfig): Promise<void>
     saveAsSync(filename: string, config: ISkiaCanvasExportConfig): void
+}
+
+export interface INapiCanvas {
+    encode(format: 'webp' | 'jpeg', quality?: number): Promise<any>
+    encode(format: 'png'): Promise<any>
+    toBuffer(mime: 'image/png'): any
+    toBuffer(mime: 'image/jpeg' | 'image/webp', quality?: number): any
+    toDataURL(mime?: 'image/png'): string
+    toDataURL(mime: 'image/jpeg' | 'image/webp', quality?: number): string
+    toDataURL(mime?: 'image/jpeg' | 'image/webp' | 'image/png', quality?: number): string
+    toDataURLAsync(mime?: 'image/png'): Promise<string>
+    toDataURLAsync(mime: 'image/jpeg' | 'image/webp', quality?: number): Promise<string>
+    toDataURLAsync(mime?: 'image/jpeg' | 'image/webp' | 'image/png', quality?: number): Promise<string>
 }
 
 export interface ISkiaCanvasExportConfig {
--- packages/platform/node/src/index.ts
+++ packages/platform/node/src/index.ts
@@ -4,12 +4,15 @@ export * from '@leafer/partner'
 export * from '@leafer/canvas-node'
 export * from '@leafer/image-web'
 
-import { ICanvasType, ICreator, IExportFileType, IExportImageType, IFunction, IObject, ISkiaCanvas } from '@leafer/interface'
+import { ICanvasType, ICreator, IExportFileType, IExportImageType, IFunction, IObject, ISkiaCanvas, INapiCanvas } from '@leafer/interface'
 import { Platform, Creator } from '@leafer/core'
 
 import { LeaferCanvas } from '@leafer/canvas-node'
 import { LeaferImage } from '@leafer/image-node'
 import { InteractionBase } from '@leafer/interaction'
+// napi-canvas
+import {promises} from "fs";
+import {join} from "path";
 
 
 Object.assign(Creator, {
@@ -30,6 +33,20 @@ export function useCanvas(canvasType: ICanvasType, power: IObject): void {
                 canvasToDataURL: (canvas: ISkiaCanvas, type?: IExportImageType, quality?: number) => canvas.toDataURLSync(type, { quality }),
                 canvasToBolb: (canvas: ISkiaCanvas, type?: IExportFileType, quality?: number) => canvas.toBuffer(type, { quality }),
                 canvasSaveAs: (canvas: ISkiaCanvas, filename: string, quality?: any) => canvas.saveAs(filename, { quality }),
+                loadImage
+            }
+        }
+
+        if (canvasType === 'napi') {
+            const { Canvas, loadImage } = power
+            Platform.origin = {
+                createCanvas: (width: number, height: number, format?: string) => new Canvas(width, height, format),
+                canvasToDataURL: (canvas: INapiCanvas, type?: IExportImageType, quality?: number) => canvas.toDataURL(`image/${type == "jpg" ? "jpeg" : type}`, quality),
+                canvasToBolb: (canvas: INapiCanvas, type?: IExportImageType, quality?: number) => {
+                    if (type == "png") return canvas.toBuffer('image/png')
+                    return canvas.toBuffer(`image/${type == "jpg" ? "jpeg" : type}`, quality)
+                },
+                canvasSaveAs: (canvas: INapiCanvas, filename: string, quality?: any) => canvas.encode("png").then(data => promises.writeFile(join(__dirname, filename), data)),
                 loadImage
             }
         }
