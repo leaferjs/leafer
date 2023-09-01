969b84ef3bc42683e762b7d3bb0dcf0a86865025
leaferjs <leaferjs@163.com>
2023-09-01 17:29:28 +0800

    curve points

--- packages/path/src/PathCommandDataHelper.ts
+++ packages/path/src/PathCommandDataHelper.ts
@@ -4,8 +4,9 @@ import { BezierHelper } from './BezierHelper'
 import { MathHelper } from '@leafer/math'
 
 
+const { sqrt, pow } = Math
 const { M, L, C, Q, Z, N, D, X, G, F, O, P, U } = PathCommandMap
-const startPoint = {} as IPointData
+const startPoint = {} as IPointData, a = {} as IPointData, b = {} as IPointData, c = {} as IPointData, c1 = {} as IPointData, c2 = {} as IPointData
 
 export const PathCommandDataHelper = {
 
@@ -73,6 +74,12 @@ export const PathCommandDataHelper = {
         }
     },
 
+    arcTo(data: IPathCommandData, x1: number, y1: number, x2: number, y2: number, radius: number): void {
+        data.push(U, x1, y1, x2, y2, radius)
+    },
+
+    // new
+
     moveToEllipse(data: IPathCommandData, x: number, y: number, radiusX: number, radiusY: number, rotation?: number, startAngle?: number, endAngle?: number, anticlockwise?: boolean): void {
         if (rotation === undefined) rotation = 0
         if (startAngle === undefined) startAngle = 0
@@ -90,20 +97,68 @@ export const PathCommandDataHelper = {
         arc(data, x, y, radius, startAngle, endAngle, anticlockwise)
     },
 
-    arcTo(data: IPathCommandData, x1: number, y1: number, x2: number, y2: number, radius: number): void {
-        data.push(U, x1, y1, x2, y2, radius)
-    },
+    points(data: IPathCommandData, points: number[], curve?: boolean | number, close?: boolean): void {
+        if (curve && points.length > 5) {
 
-    // new
-    points(data: IPathCommandData, point: number[], _curve: number): void {
-        for (let i = 0, len = point.length; i < len; i += 2) {
-            if (i === 0) {
-                data.push(M, point[i], point[i + 1])
-            } else {
-                data.push(L, point[i], point[i + 1])
+            let ba: number, cb: number, d: number, len = points.length, t = curve === true ? 0.5 : curve as number
+
+            data.push(M, points[0], points[1])
+
+            if (close) {
+                points = [points[len - 2], points[len - 1], ...points, points[0], points[1], points[2], points[3]]
+                len = points.length
             }
+
+            for (let i = 2; i < len - 2; i += 2) {
+                a.x = points[i - 2]
+                a.y = points[i - 1]
+
+                b.x = points[i]
+                b.y = points[i + 1]
+
+                c.x = points[i + 2]
+                c.y = points[i + 3]
+
+                ba = sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2))
+                cb = sqrt(pow(c.x - b.x, 2) + pow(c.y - b.y, 2))
+
+                d = ba + cb
+                ba = (t * ba) / d
+                cb = (t * cb) / d
+
+                c.x -= a.x
+                c.y -= a.y
+
+                c1.x = b.x - ba * c.x
+                c1.y = b.y - ba * c.y
+
+                if (i === 2) {
+                    if (!close) data.push(Q, c1.x, c1.y, b.x, b.y)
+                } else {
+                    data.push(C, c2.x, c2.y, c1.x, c1.y, b.x, b.y)
+                }
+
+                c2.x = b.x + cb * c.x
+                c2.y = b.y + cb * c.y
+            }
+
+            if (!close) data.push(Q, c2.x, c2.y, points[len - 2], points[len - 1])
+
+        } else {
+
+            for (let i = 0, len = points.length; i < len; i += 2) {
+                if (i === 0) {
+                    data.push(M, points[i], points[i + 1])
+                } else {
+                    data.push(L, points[i], points[i + 1])
+                }
+            }
+
         }
+
+        if (close) data.push(Z)
     }
+
 }
 
 const { ellipse, arc } = PathCommandDataHelper
