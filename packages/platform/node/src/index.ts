1112162ce5395ac371ad2726cd473a5bcf53c15b
leaferjs <leaferjs@163.com>
2023-12-29 09:52:09 +0800

    update node

--- packages/platform/node/src/index.ts
+++ packages/platform/node/src/index.ts
@@ -3,10 +3,10 @@ export * from '@leafer/core'
 export * from '@leafer/partner'
 
 export * from '@leafer/canvas-node'
-export * from '@leafer/image-web'
+export * from '@leafer/image-node'
 
 import { ICanvasType, ICreator, IExportFileType, IExportImageType, IFunction, IObject, ISkiaCanvas, ISkiaNAPICanvas } from '@leafer/interface'
-import { Platform, Creator, InteractionBase, FileHelper } from '@leafer/core'
+import { Platform, Creator, FileHelper } from '@leafer/core'
 
 import { LeaferCanvas } from '@leafer/canvas-node'
 import { LeaferImage } from '@leafer/image-node'
@@ -19,10 +19,7 @@ const { mineType, fileType } = FileHelper
 
 Object.assign(Creator, {
     canvas: (options?, manager?) => new LeaferCanvas(options, manager),
-    image: (options) => new LeaferImage(options),
-    hitCanvas: (options?, manager?) => new LeaferCanvas(options, manager),
-
-    interaction: (target, canvas, selector, options?) => { return new InteractionBase(target, canvas, selector, options) }
+    image: (options) => new LeaferImage(options)
 } as ICreator)
 
 
