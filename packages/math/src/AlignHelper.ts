d89e28c98b8b8caffef375606824dd2ff015d693
leaferjs <leaferjs@163.com>
2024-05-11 08:56:07 +0800

    update

--- packages/math/src/AlignHelper.ts
+++ packages/math/src/AlignHelper.ts
@@ -4,7 +4,7 @@ import { AroundHelper } from './AroundHelper'
 const { toPoint } = AroundHelper
 
 export const AlignHelper = {
-    toPoint(align: IAlign, alignBounds: IBoundsData, bounds: IBoundsData, to?: IPointData, onlySize?: boolean): void {
-        toPoint(align, bounds, to, onlySize, alignBounds)
+    toPoint(align: IAlign, contentBounds: IBoundsData, bounds: IBoundsData, to?: IPointData, onlySize?: boolean): void {
+        toPoint(align, bounds, to, onlySize, contentBounds)
     }
 }
