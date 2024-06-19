2fea43ae8d87fc3bdc6c3516ed98913d377f5eff
leaferjs <leaferjs@163.com>
2024-06-19 15:43:21 +0800

    update leafer type

--- packages/interface/src/app/ILeafer.ts
+++ packages/interface/src/app/ILeafer.ts
@@ -17,7 +17,7 @@ import { IControl } from '../control/IControl'
 import { IFunction } from '../function/IFunction'
 
 
-export type ILeaferType = 'draw' | 'design' | 'board' | 'document' | 'app' | 'website' | 'game' | 'player' | 'chart'
+export type ILeaferType = 'draw' | 'editor' | 'design' | 'board' | 'document' | 'app' | 'website' | 'game' | 'player' | 'chart' | 'custom'
 export interface ILeaferConfig extends IRendererConfig, ILeaferCanvasConfig, IInteractionConfig, ILayouterConfig {
     start?: boolean
     type?: ILeaferType
