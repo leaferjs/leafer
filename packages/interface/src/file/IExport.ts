d808efab70c0748929c83d6fedfbe016c4c15ccc
leaferjs <leaferjs@163.com>
2024-06-21 13:57:07 +0800

    export options add smooth / contextSettings

--- packages/interface/src/file/IExport.ts
+++ packages/interface/src/file/IExport.ts
@@ -1,19 +1,21 @@
 import { IBlob, ILeaferCanvas } from '../canvas/ILeaferCanvas'
+import { ICanvasContext2DSettings } from '../canvas/ICanvas'
 import { ILeaf } from '../display/ILeaf'
 import { ILocationType } from '../layout/ILeafLayout'
 import { IBoundsData } from '../math/IMath'
-
 
 export interface IExportOptions {
     quality?: number
     blob?: boolean
     scale?: number
+    smooth?: boolean
     pixelRatio?: number
     slice?: boolean
     trim?: boolean
     fill?: string
     screenshot?: IBoundsData | boolean
     relative?: ILocationType | ILeaf
+    contextSettings?: ICanvasContext2DSettings
     onCanvas?: IExportOnCanvasFunction
 }
 
