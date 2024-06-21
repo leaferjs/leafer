05e8f1924581ccd4aa5ed307ada142219e3d9241
leaferjs <leaferjs@163.com>
2024-06-21 23:16:56 +0800

    v1.0.0-rc.28

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -22,5 +22,5 @@ export * from '@leafer/platform'
 
 export * from '@leafer/task'
 
-export const version = "1.0.0-rc.27"
+export const version = "1.0.0-rc.28"
 export const inviteCode: IObject = {}
