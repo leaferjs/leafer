052881640fd32b90466f3c6a0f39b0dced9debb2
leaferjs <leaferjs@163.com>
2024-07-30 08:48:45 +0800

    PathCreator support auto paint

--- packages/path/src/PathCreator.ts
+++ packages/path/src/PathCreator.ts
@@ -27,6 +27,7 @@ export class PathCreator implements IPathCreator { //  tip: rewrited Pen
 
     public beginPath(): PathCreator {
         beginPath(this.__path)
+        this.paint()
         return this
     }
 
@@ -34,26 +35,31 @@ export class PathCreator implements IPathCreator { //  tip: rewrited Pen
 
     public moveTo(x: number, y: number): PathCreator {
         moveTo(this.__path, x, y)
+        this.paint()
         return this
     }
 
     public lineTo(x: number, y: number): PathCreator {
         lineTo(this.__path, x, y)
+        this.paint()
         return this
     }
 
     public bezierCurveTo(x1: number, y1: number, x2: number, y2: number, x: number, y: number): PathCreator {
         bezierCurveTo(this.__path, x1, y1, x2, y2, x, y)
+        this.paint()
         return this
     }
 
     public quadraticCurveTo(x1: number, y1: number, x: number, y: number): PathCreator {
         quadraticCurveTo(this.__path, x1, y1, x, y)
+        this.paint()
         return this
     }
 
     public closePath(): PathCreator {
         closePath(this.__path)
+        this.paint()
         return this
     }
 
@@ -61,26 +67,31 @@ export class PathCreator implements IPathCreator { //  tip: rewrited Pen
 
     public rect(x: number, y: number, width: number, height: number): PathCreator {
         rect(this.__path, x, y, width, height)
+        this.paint()
         return this
     }
 
     public roundRect(x: number, y: number, width: number, height: number, cornerRadius: number | number[]): PathCreator {
         roundRect(this.__path, x, y, width, height, cornerRadius)
+        this.paint()
         return this
     }
 
     public ellipse(x: number, y: number, radiusX: number, radiusY: number, rotation?: number, startAngle?: number, endAngle?: number, anticlockwise?: boolean): PathCreator {
         ellipse(this.__path, x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise)
+        this.paint()
         return this
     }
 
     public arc(x: number, y: number, radius: number, startAngle?: number, endAngle?: number, anticlockwise?: boolean): PathCreator {
         arc(this.__path, x, y, radius, startAngle, endAngle, anticlockwise)
+        this.paint()
         return this
     }
 
     public arcTo(x1: number, y1: number, x2: number, y2: number, radius: number): PathCreator {
         arcTo(this.__path, x1, y1, x2, y2, radius)
+        this.paint()
         return this
     }
 
@@ -88,19 +99,24 @@ export class PathCreator implements IPathCreator { //  tip: rewrited Pen
 
     public drawEllipse(x: number, y: number, radiusX: number, radiusY: number, rotation?: number, startAngle?: number, endAngle?: number, anticlockwise?: boolean): PathCreator {
         drawEllipse(this.__path, x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise)
+        this.paint()
         return this
     }
 
     public drawArc(x: number, y: number, radius: number, startAngle?: number, endAngle?: number, anticlockwise?: boolean): PathCreator {
         drawArc(this.__path, x, y, radius, startAngle, endAngle, anticlockwise)
+        this.paint()
         return this
     }
 
     public drawPoints(points: number[], curve?: boolean | number, close?: boolean): PathCreator {
         drawPoints(this.__path, points, curve, close)
+        this.paint()
         return this
     }
 
     public clearPath = this.beginPath
+
+    public paint(): void { }
 
 }
