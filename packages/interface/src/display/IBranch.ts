a757f8659373932d65dbf45b647c11a041c2bf66
leaferjs <leaferjs@163.com>
2024-09-25 22:49:00 +0800

    update

--- packages/interface/src/display/IBranch.ts
+++ packages/interface/src/display/IBranch.ts
@@ -1,11 +1,11 @@
-import { ILeaf } from './ILeaf'
+import { ILeaf, ILeafInputData } from './ILeaf'
 import { ILeaferCanvas } from '../canvas/ILeaferCanvas'
 import { IRenderOptions } from '../renderer/IRenderer'
 
 export interface IBranch extends ILeaf {
     children: ILeaf[]
     __renderBranch?(canvas: ILeaferCanvas, options: IRenderOptions): void
-    addMany(...children: ILeaf[]): void
+    addMany(...children: ILeaf[] | ILeafInputData[]): void
     removeAll(destroy?: boolean): void
     clear(): void
 }
