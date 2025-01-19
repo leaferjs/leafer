2b74581303302085e1aa44da84453cf968e9aa7a
leaferjs <leaferjs@163.com>
2025-01-19 19:11:28 +0800

    feat(export): 导出图片支持bmp格式

--- packages/interface/src/file/IFileType.ts
+++ packages/interface/src/file/IFileType.ts
@@ -1,2 +1,2 @@
-export type IExportImageType = 'jpg' | 'png' | 'webp'
+export type IExportImageType = 'jpg' | 'png' | 'webp' | 'bmp'
 export type IExportFileType = IExportImageType | 'svg' | 'pdf' | 'json'
