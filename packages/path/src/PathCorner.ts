acfc4806204c20d914281058480aff58a11e41e1
leaferjs <leaferjs@163.com>
2025-03-31 11:43:57 +0800

    fix(cornerRadius): 修复非折线路径使用圆角的循环问题

--- packages/path/src/PathCorner.ts
+++ packages/path/src/PathCorner.ts
@@ -1,7 +1,7 @@
 import { IPathCommandData } from '@leafer/interface'
 import { PointHelper } from '@leafer/math'
 
-import { PathCommandMap as Command } from './PathCommandMap'
+import { PathCommandMap as Command, PathNumberCommandLengthMap } from './PathCommandMap'
 import { PathCommandDataHelper } from './PathCommandDataHelper'
 
 
@@ -12,7 +12,7 @@ const { arcTo } = PathCommandDataHelper
 export const PathCorner = {
 
     smooth(data: IPathCommandData, cornerRadius: number, _cornerSmoothing?: number): IPathCommandData {
-        let command: number
+        let command: number, commandLen
         let i = 0, x = 0, y = 0, startX = 0, startY = 0, secondX = 0, secondY = 0, lastX = 0, lastY = 0
 
         const len = data.length
@@ -50,16 +50,15 @@ export const PathCorner = {
                     lastX = x
                     lastY = y
                     break
-                case C:  //bezierCurveTo(x1, y1, x2, y2, x, y)
-                    smooth.push(C, data[i + 1], data[i + 2], data[i + 3], data[i + 4], data[i + 5], data[i + 6])
-                    i += 7
-                    break
                 case Z:  //closepath()
                     arcTo(smooth, startX, startY, secondX, secondY, cornerRadius, lastX, lastY) // use arcTo(x1, y1, x2, y2, radius)
                     smooth.push(Z)
                     i += 1
                     break
-
+                default:
+                    commandLen = PathNumberCommandLengthMap[command]
+                    for (let j = 0; j < commandLen; j++) smooth.push(data[i + j])
+                    i += commandLen
             }
         }
 
