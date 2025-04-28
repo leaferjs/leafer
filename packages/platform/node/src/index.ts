cb682fd9a70968c359618b59b6a5ea637ab0b885
leaferjs <leaferjs@163.com>
2025-04-28 15:52:17 +0800

    refactor(platform): 独立leafer平台包，用于全量包发布

--- packages/platform/node/src/index.ts
+++ packages/platform/node/src/index.ts
@@ -1,73 +1,24 @@
-export * from '@leafer/core'
+export * from '@leafer-ui/node'
 
-export * from '@leafer/canvas-node'
-export * from '@leafer/image-node'
+export * from '@leafer-in/editor'
+export * from '@leafer-in/viewport'
+export * from '@leafer-in/view'
+export * from '@leafer-in/scroll'
 
-import { ICanvasType, ICreator, IExportFileType, IExportImageType, IFunction, IObject, ISkiaCanvas, ISkiaNAPICanvas } from '@leafer/interface'
-import { Platform, Creator, FileHelper, defineKey } from '@leafer/core'
+export * from '@leafer-in/arrow'
+export * from '@leafer-in/flow'
 
-import { LeaferCanvas } from '@leafer/canvas-node'
-import { LeaferImage } from '@leafer/image-node'
+export * from '@leafer-in/animate'
+export * from '@leafer-in/motion-path'
+export * from '@leafer-in/state'
+export * from '@leafer-in/robot'
 
-import { writeFileSync } from 'fs'
+export * from '@leafer-in/find'
+export * from '@leafer-in/export'
 
+export * from '@leafer-in/filter'
 
-const { mineType, fileType } = FileHelper
+export * from '@leafer-in/color'
+export * from '@leafer-in/resize'
 
-
-Object.assign(Creator, {
-    canvas: (options?, manager?) => new LeaferCanvas(options, manager),
-    image: (options) => new LeaferImage(options)
-} as ICreator)
-
-
-export function useCanvas(canvasType: ICanvasType, power: IObject): void {
-
-    Platform.canvasType = canvasType
-
-    if (!Platform.origin) {
-        if (canvasType === 'skia') {
-
-            const { Canvas, loadImage } = power
-            Platform.origin = {
-                createCanvas: (width: number, height: number, format?: string) => new Canvas(width, height, format),
-                canvasToDataURL: (canvas: ISkiaCanvas, type?: IExportImageType, quality?: number) => canvas.toDataURLSync(type, { quality }),
-                canvasToBolb: (canvas: ISkiaCanvas, type?: IExportFileType, quality?: number) => canvas.toBuffer(type, { quality }),
-                canvasSaveAs: (canvas: ISkiaCanvas, filename: string, quality?: number) => canvas.saveAs(filename, { quality }),
-                download(_url: string, _filename: string): Promise<void> { return undefined },
-                loadImage(src: any) { return loadImage(Platform.image.getRealURL(src)) }
-            }
-
-            Platform.roundRectPatch = true
-
-        } else if (canvasType === 'napi') {
-
-            const { Canvas, loadImage } = power
-            Platform.origin = {
-                createCanvas: (width: number, height: number, format?: string) => new Canvas(width, height, format),
-                canvasToDataURL: (canvas: ISkiaNAPICanvas, type?: IExportImageType, quality?: number) => canvas.toDataURL(mineType(type), quality),
-                canvasToBolb: async (canvas: ISkiaNAPICanvas, type?: IExportFileType, quality?: number) => canvas.toBuffer(mineType(type), quality),
-                canvasSaveAs: async (canvas: ISkiaNAPICanvas, filename: string, quality?: number) => writeFileSync(filename, canvas.toBuffer(mineType(fileType(filename)), quality)),
-                download(_url: string, _filename: string): Promise<void> { return undefined },
-                loadImage(src: any) { return loadImage(Platform.image.getRealURL(src)) }
-            }
-
-        }
-
-        Platform.ellipseToCurve = true
-
-        Platform.event = {
-            stopDefault(_origin: IObject): void { },
-            stopNow(_origin: IObject): void { },
-            stop(_origin: IObject): void { }
-        }
-
-        Platform.canvas = Creator.canvas()
-    }
-}
-
-Platform.name = 'node'
-Platform.backgrounder = true
-Platform.requestRender = function (render: IFunction): void { setTimeout(render, 16) }
-defineKey(Platform, 'devicePixelRatio', { get() { return 1 } })
-Platform.conicGradientSupport = true
+export * from '@leafer-in/interface'
--- packages/platform/web/src/index.ts
+++ packages/platform/web/src/index.ts
@@ -1,102 +1,26 @@
-export * from '@leafer/core'
+export * from 'leafer-ui'
 
-export * from '@leafer/canvas-web'
-export * from '@leafer/image-web'
+export * from '@leafer-in/editor'
+export * from '@leafer-in/text-editor'
+export * from '@leafer-in/viewport'
+export * from '@leafer-in/view'
+export * from '@leafer-in/scroll'
 
-import { ICreator, IFunction, IExportImageType, IExportFileType, IObject, ICanvasType } from '@leafer/interface'
-import { Platform, Creator, FileHelper, defineKey } from '@leafer/core'
+export * from '@leafer-in/html'
+export * from '@leafer-in/arrow'
+export * from '@leafer-in/flow'
 
-import { LeaferCanvas } from '@leafer/canvas-web'
-import { LeaferImage } from '@leafer/image-web'
+export * from '@leafer-in/animate'
+export * from '@leafer-in/motion-path'
+export * from '@leafer-in/state'
+export * from '@leafer-in/robot'
 
+export * from '@leafer-in/find'
+export * from '@leafer-in/export'
 
-const { mineType, fileType } = FileHelper
+export * from '@leafer-in/filter'
 
-Object.assign(Creator, {
-    canvas: (options?, manager?) => new LeaferCanvas(options, manager),
-    image: (options) => new LeaferImage(options)
-} as ICreator)
+export * from '@leafer-in/color'
+export * from '@leafer-in/resize'
 
-
-export function useCanvas(_canvasType: ICanvasType, _power?: IObject): void {
-    Platform.origin = {
-        createCanvas(width: number, height: number): HTMLCanvasElement {
-            const canvas = document.createElement('canvas')
-            canvas.width = width
-            canvas.height = height
-            return canvas
-        },
-        canvasToDataURL: (canvas: HTMLCanvasElement, type?: IExportImageType, quality?: number) => {
-            const imageType = mineType(type), url = canvas.toDataURL(imageType, quality)
-            return imageType === 'image/bmp' ? url.replace('image/png;', 'image/bmp;') : url
-        },
-        canvasToBolb: (canvas: HTMLCanvasElement, type?: IExportFileType, quality?: number) => new Promise((resolve) => canvas.toBlob(resolve, mineType(type), quality)),
-        canvasSaveAs: (canvas: HTMLCanvasElement, filename: string, quality?: any) => {
-            const url = canvas.toDataURL(mineType(fileType(filename)), quality)
-            return Platform.origin.download(url, filename)
-        },
-        download(url: string, filename: string): Promise<void> {
-            return new Promise((resolve) => {
-                let el = document.createElement('a')
-                el.href = url
-                el.download = filename
-                document.body.appendChild(el)
-                el.click()
-                document.body.removeChild(el)
-                resolve()
-            })
-        },
-        loadImage(src: any): Promise<HTMLImageElement> {
-            return new Promise((resolve, reject) => {
-                const img = new Platform.origin.Image()
-                const { crossOrigin } = Platform.image
-                if (crossOrigin) {
-                    img.setAttribute('crossOrigin', crossOrigin)
-                    img.crossOrigin = crossOrigin
-                }
-                img.onload = () => { resolve(img) }
-                img.onerror = (e: any) => { reject(e) }
-                img.src = Platform.image.getRealURL(src)
-            })
-        },
-        Image,
-        PointerEvent,
-        DragEvent
-    }
-
-    Platform.event = {
-        stopDefault(origin: Event): void { origin.preventDefault() },
-        stopNow(origin: Event): void { origin.stopImmediatePropagation() },
-        stop(origin: Event): void { origin.stopPropagation() }
-    }
-
-    Platform.canvas = Creator.canvas()
-    Platform.conicGradientSupport = !!Platform.canvas.context.createConicGradient
-}
-
-Platform.name = 'web'
-Platform.isMobile = 'ontouchstart' in window
-Platform.requestRender = function (render: IFunction): void { window.requestAnimationFrame(render) }
-defineKey(Platform, 'devicePixelRatio', { get() { return devicePixelRatio } })
-
-
-// same as worker
-
-const { userAgent } = navigator
-
-if (userAgent.indexOf("Firefox") > -1) {
-    Platform.conicGradientRotate90 = true
-    Platform.intWheelDeltaY = true
-    Platform.syncDomFont = true
-} else if (userAgent.indexOf("AppleWebKit") > -1) {
-    Platform.fullImageShadow = true // 苹果内核渲染阴影
-}
-
-if (userAgent.indexOf('Windows') > -1) {
-    Platform.os = 'Windows'
-    Platform.intWheelDeltaY = true
-} else if (userAgent.indexOf('Mac') > -1) {
-    Platform.os = 'Mac'
-} else if (userAgent.indexOf('Linux') > -1) {
-    Platform.os = 'Linux'
-}
+export * from '@leafer-in/interface'
